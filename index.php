811b49d11948c050c1905d02728e8996a8523fcd
inihelta <helta3452@gmail.com>
2026-01-13 11:09:34 +0800

    tambahan dari ka mela

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php include "koneksi.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>halaman pertama</h1>
+    <table border="1">
+        <tr>
+            <th>No.</th>
+            <th>Nama</th>
+            <th>Kelas</th>
+            <th>Jurusan</th>
+            <th>Aksi</th>
+        </tr>
+        <?php
+        $no = 1;
+        $siswa = mysqli_query($koneksi, "SELECT * FROM siswa ORDER BY id DESC");
+        if (mysqli_num_rows($siswa) > 0) {
+            while ($row = mysqli_fetch_assoc($siswa)) {
+        ?>
+                <tr>
+                    <td><?php echo $no++; ?></td>
+                    <td><?php echo $row['nama']; ?></td>
+                    <td><?php echo $row['kelas']; ?></td>
+                    <td><?php echo $row['jurusan']; ?></td>
+                    <td><a href='edit.php?id=<?php echo $row['id']; ?>'>Edit</a> || <a href='hapus.php?id=<?php echo $row['id']; ?>'>Hapus</a></td onclick="return confirm('Yakin ingin menghapus data ini?')">
+                </tr>
+            <?php
+            }
+        } else { ?>
+            <tr>
+                <td colspan="5">Tidak ada data</td>
+            </tr>
+
+        <?php } ?>
+    </table>
+</body>
+
+</html>
---
+++ tambah.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tambah data</title>
+</head>
+
+<body>
+    <h1>Halaman Tambah Data</h1>
+    <br>
+    <form action="tambah_proses.php" method="post">
+        <table>
+            <tr>
+                <td>Nama</td>
+                <td><input type="text" name="nama" placeholder="Masukkan Nama"></td>
+            </tr>
+            <tr>
+                <td>Kelas</td>
+                <td><input type="text" name="kelas" placeholder="Masukkan kelas"></td>
+            </tr>
+            <tr>
+                <td>Jurusan</td>
+                <td><input type="text" name="jurusan" placeholder="Masukkan Jurusan"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" value="TAMBAH"></td>
+            </tr>
+        </table>
+</body>
+
+</html>
